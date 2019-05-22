058d6c3bbacd32535b1240f5fe95813dc26389b2
Lyhoshva Vlad <lyhoshvavlad@gmail.com>
2019-05-23 00:35:25 +0300

    Added URI parsing

--- app/Application.php
+++ app/Application.php
@@ -11,6 +11,17 @@ class Application
 {
     public function run()
     {
+        $uri = $this->parseUri();
+    }
 
+    public function parseUri()
+    {
+        preg_match('~^/([\d\w\.]+)\/?(\d+)?\??([\d\w\.=]+)?$~', $_SERVER['REQUEST_URI'], $matches);
+        parse_str($matches[3], $params);
+        return [
+            'action' => $matches[1],
+            'id' => $matches[2],
+            'params' => $params,
+        ];
     }
 }
